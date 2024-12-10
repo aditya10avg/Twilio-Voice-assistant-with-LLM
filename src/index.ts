8481dd8c5973ca1f07247131edc49fa1e9fa7ce2
AdityaAVG <adityavg1005@gmail.com>
2024-12-10 09:29:28 +0530

    Continued conversation is added.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
 import { serve } from 'bun'
 import {twiml} from 'twilio'
+import OpenAI from 'openai'
+import { config } from 'dotenv'
 
+config()
 
 const app = new Hono()
-
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! })
 
 app.post('/call', (c) => {
   const Voicemessage= new twiml.VoiceResponse();
@@ -22,11 +25,27 @@ app.post('/call', (c) => {
 app.post('/respond',async (c) => {
   const formData = await c.req.formData()
   const voiceInput = formData.get('SpeechResult')?.toString()!
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are an experienced cold caller who qualifies potential leads by speaking with them."
+      },
+      {
+        role: "user",
+        content: voiceInput // Passing the message that comes from the user for message generation.
+      }
+    ],
+    temperature: 0.5,
+})
 
   // Here we will set up the LLM model to understand the speech input and respond accordingly.
+  const assistantResponse = chatCompletion.choices[0].message.content
 
   const Voicemessage= new twiml.VoiceResponse();
-  Voicemessage.say(voiceInput);
+  Voicemessage.say(assistantResponse!);
+  Voicemessage.redirect({method: 'POST'},'/call') // This ensure that call is hunged up and continuous conversation will start.
   c.header('Content-Type','application/xml');
   return c.body(Voicemessage.toString());
 })
